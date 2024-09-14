e74b29b16392187934b3221eb2da6c0fbc1f8c15
n00bCod3r <n00bCod3r@protonmail.com>
2024-09-14 21:03:14 +0200

    update
    refactor code, improve styles and readability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_worldHigh from "@amcharts/amcharts5-geodata/worldHigh";
 import am5geodata_usaHigh from "@amcharts/amcharts5-geodata/usaHigh";
 import am5geodata_italyHigh from "@amcharts/amcharts5-geodata/italyHigh";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import am5themes_Micro from "@amcharts/amcharts5/themes/Micro";
 import countries from "@amcharts/amcharts5-geodata/data/countries2";
 
-import { colors } from 'src/data/colors.data';
 import { albumXcountry } from 'src/data/countryxalbum.data';
 import { firstValueFrom } from 'rxjs';
 
@@ -19,113 +20,112 @@ import { firstValueFrom } from 'rxjs';
 })
 export class AppComponent {
   title = 'music-map';
+  worldSeries!: am5map.MapPolygonSeries;
+  usSeries!: am5map.MapPolygonSeries;
+  itSeries!: am5map.MapPolygonSeries;
   root!: am5.Root;
+  chart!: am5map.MapChart;
+  allSeries: am5map.MapPolygonSeries[] = [];
+  mapConfig!: am5map.IMapChartSettings
   colors: any = {};
 
   htmlTooltip: string = '';
   constructor(private http: HttpClient) { }
 
-  async ngOnInit() {
-    this.initializeColors();
 
+  async ngOnInit() {
     this.htmlTooltip = await firstValueFrom(this.http.get('assets/tooltip.html', { responseType: 'text' }));
 
     // root-init
     this.root = am5.Root.new("map"); // pass id of the <div> container
     this.root.numberFormatter.set("numberFormat", "#"); // do not add , to thousands
+    this.root.setThemes([
+      am5themes_Animated.new(this.root),
+      am5themes_Micro.new(this.root)
+    ]);
 
+    this.setMapConfig();
+    
+    // chart-init
+    this.chart = this.root.container.children.push(am5map.MapChart.new(this.root, this.mapConfig));
 
-    // MARK: map-init
-    const mapConfig: am5map.IMapChartSettings = {
-      projection: am5map.geoOrthographic(), // Globe projection
-      panX: "rotateX",
-      panY: "rotateY",
+    this.createPolygonSeries();
+    this.setChartBg();
+    this.setSeriesConfig();
+    this.setSeriesData(); // Must be the last operation performed
+  }
 
-      /// zoom settings
-      zoomControl: am5map.ZoomControl.new(this.root, {}), // Add zoom control
-      minZoomLevel: 0.1,
-      maxZoomLevel: 32
-    }
+  /**
+   * Add polygon maps to the chart
+   * @returns void
+   */
+  createPolygonSeries(): void {
 
-    const mapChart: am5map.MapChart = am5map.MapChart.new(this.root, mapConfig)
-
-    let chart = this.root.container.children.push(mapChart); // Make map visible
-
-    //todo: understand how bg works
-
-
-    //#region bg colors
-    /// Main bg
-    chart.chartContainer.set("background", am5.Rectangle.new(this.root, {
-      fill: am5.color(this.colors.bgColor),
-      fillOpacity: 0.9
-    }));
-
-    /// Sea bg
-    let backgroundSeries = chart.series.unshift(
-      am5map.MapPolygonSeries.new(this.root, {})
-    );
-
-    backgroundSeries.mapPolygons.template.setAll({
-      fill: am5.color(this.colors.seaColor),
-      stroke: am5.color(this.colors.seaColor),
-    });
-
-    backgroundSeries.data.push({
-      geometry: am5map.getGeoRectangle(90, 180, -90, -180)
-    });
-    //#endregion bg colors
-
-    //#region maps
-    let polygonSeriesWorld = chart.series.push(
+    this.worldSeries = this.chart.series.push(
       am5map.MapPolygonSeries.new(this.root, {
         geoJSON: am5geodata_worldHigh,
       })
     );
 
-    // let polygonSeriesUSA = chart.series.push(
-    //   am5map.MapPolygonSeries.new(this.root, {
-    //     geoJSON: am5geodata_usaHigh,
-    //   })
-    // );
+    this.usSeries = this.chart.series.push(
+      am5map.MapPolygonSeries.new(this.root, {
+        geoJSON: am5geodata_usaHigh,
+      })
+    );
 
-    // let polygonSeriesItaly = chart.series.push(
-    //   am5map.MapPolygonSeries.new(this.root, {
-    //     geoJSON: am5geodata_italyHigh,
-    //   })
-    // );
-    //#endregion maps
+    this.itSeries = this.chart.series.push(
+      am5map.MapPolygonSeries.new(this.root, {
+        geoJSON: am5geodata_italyHigh,
+      })
+    );
 
-    // this.setPolygonSeriesProperties([polygonSeriesUSA, polygonSeriesWorld, polygonSeriesItaly]);
-    this.setPolygonSeriesProperties([polygonSeriesWorld]);
 
-    // Must be the last operation performed
-    // this.setPolygonSeriesData([polygonSeriesUSA, polygonSeriesWorld, polygonSeriesItaly]);
-    this.setData([polygonSeriesWorld]);
-
+    this.allSeries.push(this.worldSeries)
   }
 
   /**
-   * Create an obejct to easily access colors using SCSS variables
-   * @returns void
+   * Get am5 Color from css var name
+   * @param  {string} propertyName
+   * @returns am5
    */
-  initializeColors(): void {
-    Object.entries(colors).forEach(([key, cssVar]) => {
-      this.colors[key] = getComputedStyle(document.documentElement)
-        .getPropertyValue(cssVar).trim()
-    })
-    // console.log(this.colors)
+  getColor(propertyName: string): am5.Color {
+    return am5.color(getComputedStyle(document.documentElement).getPropertyValue(propertyName) || '#ff0000');
   }
 
-  setPolygonSeriesProperties(polygonSeries: am5map.MapPolygonSeries[]): void {
-    polygonSeries.forEach(series => {
+  setChartBg(): void {
+    /// Main bg
+    this.chart.chartContainer.set("background", am5.Rectangle.new(this.root, {
+      fill: this.getColor('--bg-color'),
+      fillOpacity: 0.9
+    }));
 
+    /// Sea bg
+    let backgroundSeries = this.chart.series.unshift(
+      am5map.MapPolygonSeries.new(this.root, {})
+    );
+
+    backgroundSeries.mapPolygons.template.setAll({
+      fill: this.getColor('--sea-color'),
+      stroke: this.getColor('--sea-color'),
+    });
+
+    backgroundSeries.data.push({
+      geometry: am5map.getGeoRectangle(90, 180, -90, -180)
+    });
+
+    this.allSeries.forEach(series => {series.mapPolygons.template.states.create("hover", {
+        fill: this.getColor('--hover-color'),
+      });
+    });
+  }
+
+  setSeriesConfig(): void {
+    this.allSeries.forEach(series => {
       // Set HTML popup
       series.mapPolygons.template.setAll({
         tooltipHTML: this.htmlTooltip,
         interactive: true,
       })
-
 
       //dbg log item on hover to inspect properties
       series.mapPolygons.template.events.on("pointerover", (event) => {
@@ -134,22 +134,15 @@ export class AppComponent {
           console.log(dataItem.dataContext);
         }
       });
-
-      // MARK: hover-color
-      // series.mapPolygons.template.states.create("hover", {
-      //   fill: am5.color(0x677935)
-      // });
     })
   }
 
-
   /**
    * Set data for tooltip
-   * @param  {am5map.MapPolygonSeries[]} polygonSeries
    * @returns void
    */
-  setData(polygonSeries: am5map.MapPolygonSeries[]): void {
-    polygonSeries.forEach(series => {
+  setSeriesData(): void {
+    this.allSeries.forEach(series => {
       series.data.setAll(
         // MARK: tooltip-data
         albumXcountry.map(el => {
@@ -166,4 +159,16 @@ export class AppComponent {
     })
   }
 
+  setMapConfig(): void {
+    this.mapConfig = {
+      projection: am5map.geoOrthographic(), // Globe projection
+      panX: "rotateX",
+      panY: "rotateY",
+
+      /// zoom settings
+      zoomControl: am5map.ZoomControl.new(this.root, {}), // Add zoom control
+      minZoomLevel: 0.1,
+      maxZoomLevel: 32
+    }
+  }
 }
--- src/data/colors.data.ts
+++
@@ -1,6 +0,0 @@
-export const colors = {
-  bgColor: '--bg-color',
-  seaColor: '--sea-color',
-  fontSize: '--font-size',
-  iconSize: '--icon-size',
-}
