19341cafbc27b1ca5f74af204434191cd7de8a7a
n00bCod3r <n00bCod3r@protonmail.com>
2026-07-24 02:37:04 +0200

    Updates

---
+++ assets/adata.bkp.ts
@@ -0,0 +1,270 @@
+interface AlbumInfo {
+  id: string,
+  artist: string,
+  album: string,
+  group: string,
+  name: string,
+  year: number,
+  genres: string,
+  url: string,
+}
+
+export const albums: AlbumInfo[] = [
+  // ALABAMA
+  {
+    id: 'US-AL',
+    name: 'Alabama',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // ALASKA 
+  {
+    id: 'US-AK',
+    name: 'Alaska',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // CONNECTICUT
+  {
+    id: 'US-CT',
+    name: 'Connecticut',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // DELAWARE
+  {
+    id: 'US-DE',
+    name: 'Delaware',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // HAWAII
+  {
+    id: 'US-HI',
+    name: 'Hawaii',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // IDAHO
+  {
+    id: 'US-ID',
+    name: 'Idaho',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // INDIANA
+  {
+    id: 'US-IN',
+    name: 'Indiana',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // KANSAS
+  {
+    id: 'US-KS',
+    name: 'Kansas',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // KENTUCKY
+  {
+    id: 'US-KY',
+    name: 'Kentucky',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // MAINE
+  {
+    id: 'US-ME',
+    name: 'Maine',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // MONTANA
+  {
+    id: 'US-MT',
+    name: 'Montana',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // NEW MEXICO
+  {
+    id: 'US-NM',
+    name: 'New Mexico',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // NORTH DAKOTA
+  {
+    id: 'US-ND',
+    name: 'North Dakota',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // OKLAHOMA
+  {
+    id: 'US-OK',
+    name: 'Oklahoma',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // OREGON
+  {
+    id: 'US-OR',
+    name: 'Oregon',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // RHODE ISLAND
+  {
+    id: 'US-RI',
+    name: 'Rhode Island',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // SOUTH CAROLINA
+  {
+    id: 'US-SC',
+    name: 'South Carolina',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // SOUTH DAKOTA
+  {
+    id: 'US-SD',
+    name: 'South Dakota',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+  // UTAH
+  {
+    id: 'US-UT',
+    name: 'Utah',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+
+  // VERMONT
+  {
+    id: 'US-VT',
+    name: 'Vermont',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+
+  // WEST VIRGINIA
+  {
+    id: 'US-WV',
+    name: 'West Virginia',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+
+  // WISCONSIN
+  {
+    id: 'US-WI',
+    name: 'Wisconsin',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  },
+
+  // WYOMING
+  {
+    id: 'US-WY',
+    name: 'Wyoming',
+    artist: 'foo',
+    album: 'foo',
+    group: 'USA',
+    year: 2000,
+    genres: 'foo',
+    url: 'foo'
+  }
+]
